325fe3ba0fef30de6644e2ac86db73dfc5682a21
Fatih Arslan <fatih@koding.com>
2013-09-14 20:19:48 +0300

    more improvements to dnode

--- kite/dnodecodec.go
+++ kite/dnodecodec.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"koding/tools/dnode"
+	"log"
 	"net/rpc"
 )
 
@@ -40,27 +42,61 @@ func (c *DnodeClientCodec) ReadResponseBody(x interface{}) error {
 
 func (c *DnodeClientCodec) Close() error {
 	fmt.Println("Dnode ClientClose")
-	return nil
+	return c.rwc.Close()
 }
 
-type DnodeServerCodec struct{}
+//
+
+type DnodeMessage struct {
+	Method    interface{}           `json:"method"`
+	Arguments *dnode.Partial        `json:"arguments"`
+	Callbacks map[string]([]string) `json:"callbacks"`
+}
+
+func (d *DnodeMessage) reset() {
+	d.Method = nil
+	d.Arguments = nil
+	d.Callbacks = nil
+}
+
+type DnodeServerCodec struct {
+	dec *json.Decoder // for reading JSON values
+	enc *json.Encoder // for writing JSON values
+	rwc io.ReadWriteCloser
+
+	// temporary work space
+	req  DnodeMessage
+	resp DnodeMessage
+}
+
+func NewDnodeServerCodec(conn io.ReadWriteCloser) rpc.ServerCodec {
+	return &DnodeServerCodec{
+		rwc: conn,
+		dec: json.NewDecoder(conn),
+		enc: json.NewEncoder(conn),
+	}
+}
 
 func (c *DnodeServerCodec) ReadRequestHeader(r *rpc.Request) error {
-	fmt.Println("Dnode ReadRequestHeader")
+	err := c.dec.Decode(&c.req)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Dnode Message arguments %+v\n", c.req.Arguments)
+	fmt.Printf("Dnode Message arguments %+v\n", string(c.req.Arguments.Raw))
+
 	return nil
 }
 
 func (c *DnodeServerCodec) ReadRequestBody(body interface{}) error {
-	fmt.Println("Dnode ReadRequestBody")
 	return nil
 }
 
 func (c *DnodeServerCodec) WriteResponse(r *rpc.Response, body interface{}) error {
-	fmt.Println("Dnode WriteResponse")
+	// fmt.Printf("Dnode WriteResponse r: %+v body: %+v\n", r, body)
 	return nil
 }
 
 func (c *DnodeServerCodec) Close() error {
-	fmt.Println("Dnode ServerClose")
-	return nil
+	return c.rwc.Close()
 }
