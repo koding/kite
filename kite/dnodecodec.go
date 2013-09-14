6fc91ae07abf6ac9c6c9881178ce7b9bf14fd8ce
Fatih Arslan <fatih@koding.com>
2013-09-14 16:45:09 +0300

    Fill template functions

--- kite/dnodecodec.go
+++ kite/dnodecodec.go
@@ -1,52 +1,66 @@
 // Dnode protocol for net/rpc
 package kite
 
-import "net/rpc"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/rpc"
+)
 
-/******************************************
+func NewDnodeClient(conn io.ReadWriteCloser) rpc.ClientCodec {
+	return &DnodeClientCodec{
+		rwc: conn,
+		dec: json.NewDecoder(conn),
+		enc: json.NewEncoder(conn),
+	}
+}
 
-Client
+type DnodeClientCodec struct {
+	dec *json.Decoder
+	enc *json.Encoder
+	rwc io.ReadWriteCloser
+}
 
-******************************************/
+func (c *DnodeClientCodec) WriteRequest(r *rpc.Request, body interface{}) error {
+	fmt.Println("Dnode WriteRequest")
 
-type DnodeClientCodec struct{}
-
-func (c *DnodeClientCodec) WriteRequest(r *rpc.Request, param interface{}) error {
 	return nil
 }
 
 func (c *DnodeClientCodec) ReadResponseHeader(r *rpc.Response) error {
+	fmt.Println("Dnode ReadResponseHeader")
 	return nil
 }
 
 func (c *DnodeClientCodec) ReadResponseBody(x interface{}) error {
+	fmt.Println("Dnode ReadResponseBody")
 	return nil
 }
 
 func (c *DnodeClientCodec) Close() error {
+	fmt.Println("Dnode ClientClose")
 	return nil
 }
-
-/******************************************
-
-SERVER
-
-******************************************/
 
 type DnodeServerCodec struct{}
 
 func (c *DnodeServerCodec) ReadRequestHeader(r *rpc.Request) error {
+	fmt.Println("Dnode ReadRequestHeader")
 	return nil
 }
 
-func (c *DnodeServerCodec) ReadRequestBody(x interface{}) error {
+func (c *DnodeServerCodec) ReadRequestBody(body interface{}) error {
+	fmt.Println("Dnode ReadRequestBody")
 	return nil
 }
 
-func (c *DnodeServerCodec) WriteResponse(r *rpc.Response, x interface{}) error {
+func (c *DnodeServerCodec) WriteResponse(r *rpc.Response, body interface{}) error {
+	fmt.Println("Dnode WriteResponse")
 	return nil
 }
 
 func (c *DnodeServerCodec) Close() error {
+	fmt.Println("Dnode ServerClose")
 	return nil
 }
