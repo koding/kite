e67b1d3ee5e50a3900432ae4c95e0bea9f40f9ae
Huseyin Oguz Albayrak <huseyinalb@gmail.com>
2013-09-19 18:52:53 +0300

    examples fixed

--- test/exp.go
+++ test/exp.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"koding/newkite/kite"
+	"koding/newkite/protocol"
 	"sync"
 	"time"
 )
 
+var port = flag.String("port", "", "port to bind itself")
 var work sync.WaitGroup
 
+type Exp struct {
+}
+
 func main() {
-	k := kite.New("application", nil)
+	flag.Parse()
+	methods := map[string]interface{}{}
+	o := protocol.Options{
+		Username: "huseyin",
+		Kitename: "application",
+		Port:     *port,
+	}
+	k := kite.New(&o, new(Exp), methods)
 	go k.Start()
 
 	// this is needed that the goroutine k.Start() is been settled. We will
@@ -19,7 +32,7 @@ func main() {
 	// rethought.
 	time.Sleep(1 * time.Second)
 
-	squareOf := func(i int) {
+	squareOf := func(i float64) {
 		k.Call("mathworker", "Square", i, func(err error, res string) {
 			if err != nil {
 				fmt.Println("got an error", err)
@@ -37,7 +50,7 @@ func main() {
 	// (pro-tip: start multiple mathworkers to distribute the work between them)
 	for i := 0; i <= 10; i++ {
 		work.Add(1)
-		go squareOf(i)
+		go squareOf(float64(i))
 	}
 
 	// wait now until all of our work is done :)
