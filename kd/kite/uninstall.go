1f066dbce3f7c3283030587a10e013fa6a704f6d
Cenk Alti <cenkalti@gmail.com>
2013-10-24 15:26:55 +0300

    improve uninstall command

--- kd/kite/uninstall.go
+++ kd/kite/uninstall.go
@@ -3,6 +3,7 @@ package kite
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"koding/newKite/kd/util"
 	"os"
 	"path/filepath"
@@ -21,10 +22,44 @@ func (*Uninstall) Definition() string {
 func (*Uninstall) Exec() error {
 	flag.Parse()
 	if flag.NArg() != 1 {
-		return errors.New("You should give a kite name")
+		return errors.New("You should give a full kite name. Exampe: fs-1.0.0")
 	}
-	name := flag.Arg(0)
+	fullName := flag.Arg(0)
 
-	path := filepath.Join(util.GetKdPath(), "kites", name+".kite")
-	return os.RemoveAll(path)
+	if _, _, err := splitVersion(fullName, false); err != nil {
+		return err
+	}
+
+	installed, err := isInstalled(fullName)
+	if err != nil {
+		return err
+	}
+	if !installed {
+		return fmt.Errorf("%s is not installed", fullName)
+	}
+
+	return os.RemoveAll(getBundlePath(fullName))
+}
+
+// getBundlePath returns the bundle path of a given kite.
+// Example: "adsf-1.2.3" -> "~/.kd/kites/asdf-1.2.3.kite"
+func getBundlePath(fullKiteName string) string {
+	return filepath.Join(util.GetKdPath(), "kites", fullKiteName+".kite")
+}
+
+// isInstalled returns true if the kite is installed.
+func isInstalled(fullKiteName string) (bool, error) {
+	return exists(getBundlePath(fullKiteName))
+}
+
+// exists returns whether the given file or directory exists or not.
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
