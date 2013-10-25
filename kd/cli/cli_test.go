39061b10326322f9c81f550e3f01d145a86caf84
Cenk Alti <cenkalti@gmail.com>
2013-10-25 10:49:42 +0300

    add cli tests

---
+++ kd/cli/cli_test.go
@@ -0,0 +1,66 @@
+package cli
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Hello struct{ name string }
+
+func NewHello(name string) *Hello { return &Hello{name} }
+
+func (h *Hello) Definition() string {
+	return "Say hello to " + h.name
+}
+
+func (h *Hello) Exec() error {
+	fmt.Println("hello")
+	return nil
+}
+
+func TestFindCommand(t *testing.T) {
+	hello := NewHello("hello")
+	hello2 := NewHello("hello2")
+
+	root := NewCLI()
+	root.AddCommand("hello", hello)
+
+	s := root.AddSubCommand("sub")
+	s.AddCommand("hello2", hello2)
+
+	println("==========")
+	_, err := root.findCommand([]string{"notExist"})
+	if err == nil {
+		t.Error("error")
+	}
+
+	println("==========")
+	cmd, err := root.findCommand([]string{"hello"})
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if cmd.Definition() != hello.Definition() {
+		t.Error("error")
+	}
+
+	println("==========")
+	_, err = s.findCommand([]string{"notExist"})
+	if err == nil {
+		t.Error("error")
+	}
+
+	println("==========")
+	cmd, err = root.findCommand([]string{"sub", "hello2"})
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if cmd.Definition() != hello2.Definition() {
+		t.Error("error")
+	}
+
+	println("==========")
+	_, err = root.findCommand([]string{"sub", "notExist"})
+	if err == nil {
+		t.Error("error")
+	}
+}
