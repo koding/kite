1129962315f931c4614f237ca2bf90df9264faf6
Cenk Alti <cenkalti@gmail.com>
2013-10-25 13:42:23 +0300

    extract getInstalledKites function

--- kd/kite/list.go
+++ kd/kite/list.go
@@ -20,24 +20,43 @@ func (*List) Definition() string {
 }
 
 func (*List) Exec(args []string) error {
+	kites, err := getInstalledKites()
+	if err != nil {
+		return err
+	}
+
+	for _, k := range kites {
+		fmt.Println(k)
+	}
+
+	return nil
+}
+
+func getInstalledKites() ([]string, error) {
+	installedKites := []string{} // to be returned
 	kitesPath := filepath.Join(util.GetKdPath(), "kites")
+
 	kites, err := ioutil.ReadDir(kitesPath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return nil
+			return []string{}, nil
 		}
-		return err
+
+		return nil, err
 	}
+
 	for _, fi := range kites {
 		name := fi.Name()
 		if !strings.HasSuffix(name, ".kite") {
 			continue
 		}
-		name = strings.TrimSuffix(name, ".kite")
-		name, version, err := splitVersion(name, false)
+
+		fullName := strings.TrimSuffix(name, ".kite")
+		name, _, err := splitVersion(fullName, false)
 		if err == nil {
-			fmt.Println(name + "-" + version)
+			installedKites = append(installedKites, fullName)
 		}
 	}
-	return nil
+
+	return installedKites, nil
 }
