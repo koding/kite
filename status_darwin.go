8774a865f0401e4a869d0e0cf78348fb8775eefa
Cenk Alti <cenkalti@gmail.com>
2014-01-26 07:11:21 -0800

    implement memory and disk usage info without gosigar

---
+++ status_darwin.go
@@ -0,0 +1,95 @@
+package kite
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"syscall"
+	"unsafe"
+)
+
+func memoryStats() (*memory, error) {
+	m := new(memory)
+
+	if err := sysctlbyname("hw.memsize", &m.Total); err != nil {
+		return nil, err
+	}
+
+	free, inactive, err := vm_stat()
+	if err != nil {
+		return nil, err
+	}
+
+	m.Usage = m.Total - free - inactive
+
+	return m, nil
+}
+
+func vm_stat() (bytesFree, bytesInactive uint64, err error) {
+	type memStat struct {
+		regex *regexp.Regexp
+		value uint64
+		valid bool
+	}
+
+	stats := map[string]*memStat{
+		"pageSize":      &memStat{regex: regexp.MustCompile("page size of (\\d+) bytes")},
+		"pagesFree":     &memStat{regex: regexp.MustCompile("Pages free: *(\\d+).")},
+		"pagesInactive": &memStat{regex: regexp.MustCompile("Pages inactive: *(\\d+).")},
+	}
+
+	cmd := exec.Command("vm_stat")
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Parse lines in vm_stat output
+	lines := bytes.Split(out, []byte("\n"))
+	for _, line := range lines {
+		for _, stat := range stats {
+			match := stat.regex.FindSubmatch(line)
+			if match == nil {
+				continue
+			}
+
+			stat.value, err = strconv.ParseUint(string(match[1]), 10, 64)
+			if err != nil {
+				return 0, 0, err
+			}
+			stat.valid = true
+		}
+	}
+
+	// Check every stat is found in output
+	for _, stat := range stats {
+		if !stat.valid {
+			return 0, 0, errors.New("cannot parse vm_stat output")
+		}
+	}
+
+	pageSize := stats["pageSize"].value
+	return pageSize * stats["pagesFree"].value, pageSize * stats["pagesInactive"].value, nil
+}
+
+// generic Sysctl buffer unmarshalling
+func sysctlbyname(name string, data interface{}) (err error) {
+	val, err := syscall.Sysctl(name)
+	if err != nil {
+		return err
+	}
+
+	buf := []byte(val)
+
+	switch v := data.(type) {
+	case *uint64:
+		*v = *(*uint64)(unsafe.Pointer(&buf[0]))
+		return
+	}
+
+	bbuf := bytes.NewBuffer([]byte(val))
+	return binary.Read(bbuf, binary.LittleEndian, data)
+}
---
+++ status_linux.go
@@ -0,0 +1,95 @@
+package kite
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type procMem struct {
+	Total      uint64
+	Used       uint64
+	Free       uint64
+	ActualFree uint64
+	ActualUsed uint64
+}
+
+func memoryStats() (*memory, error) {
+	m := new(memory)
+	mem := procMem{}
+
+	err := mem.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	m.Usage = mem.ActualUsed
+	m.Total = mem.Total
+
+	return m, nil
+}
+
+func (self *procMem) Get() error {
+	var buffers, cached uint64
+	table := map[string]*uint64{
+		"MemTotal": &self.Total,
+		"MemFree":  &self.Free,
+		"Buffers":  &buffers,
+		"Cached":   &cached,
+	}
+
+	if err := parseMeminfo(table); err != nil {
+		return err
+	}
+
+	self.Used = self.Total - self.Free
+	kern := buffers + cached
+	self.ActualFree = self.Free + kern
+	self.ActualUsed = self.Used - kern
+
+	return nil
+}
+
+func parseMeminfo(table map[string]*uint64) error {
+	return readFile("/proc/meminfo", func(line string) bool {
+		fields := strings.Split(line, ":")
+
+		if ptr := table[fields[0]]; ptr != nil {
+			num := strings.TrimLeft(fields[1], " ")
+			val, err := strtoull(strings.Fields(num)[0])
+			if err == nil {
+				*ptr = val * 1024
+			}
+		}
+
+		return true
+	})
+}
+
+func readFile(file string, handler func(string) bool) error {
+	contents, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	reader := bufio.NewReader(bytes.NewBuffer(contents))
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if !handler(string(line)) {
+			break
+		}
+	}
+
+	return nil
+}
+
+func strtoull(val string) (uint64, error) {
+	return strconv.ParseUint(val, 10, 64)
+}
