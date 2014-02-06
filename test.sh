23ffe772e71e4eae5c499919fe3aefe6e431b2c7
Cenk Alti <cenkalti@gmail.com>
2014-02-06 14:15:06 -0800

    use custom KITE_HOME while testing

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Do not owerwrite the user's key when running tests
+KITE_HOME=/tmp/kite_home
+rm -rf $KITE_HOME
+export KITE_HOME=$KITE_HOME
+
 go test -v ./dnode
 go test -v ./dnode/rpc
 go test -v ./cmd/cli
