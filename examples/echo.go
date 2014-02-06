4230db67282033d721f113225e6c19360b6ba6a4
Cenk Alti <cenkalti@gmail.com>
2014-02-05 23:36:08 -0800

    update echo kite

--- examples/echo.go
+++ examples/echo.go
@@ -1,40 +1,22 @@
-// Echo kite is an example to kite that just echos the argument back
-// Useful for debugging or testing stuff
+// Echo kite is an example to kite that just echoes the argument back.
+// Useful for debugging or testing stuff.
 package main
 
-import (
-	"fmt"
-	"kite"
-	"koding/tools/config"
-	"net/url"
-	"strconv"
-)
+import "kite"
 
 func main() {
-	k := newKite()
+	options := &kite.Options{
+		Kitename:    "echo",
+		Version:     "0.0.1",
+		Environment: "development",
+		Region:      "localhost",
+	}
+
+	k := kite.New(options)
+
 	k.HandleFunc("echo", func(r *kite.Request) (interface{}, error) {
 		return r.Args.One().MustString(), nil
 	})
 
 	k.Run()
-}
-
-func newKite() *kite.Kite {
-	kontrolPort := strconv.Itoa(config.Current.NewKontrol.Port)
-	kontrolHost := config.Current.NewKontrol.Host
-	kontrolURL := &url.URL{
-		Scheme: "ws",
-		Host:   fmt.Sprintf("%s:%s", kontrolHost, kontrolPort),
-		Path:   "/dnode",
-	}
-
-	options := &kite.Options{
-		Kitename:    "echo",
-		Environment: config.FileProfile,
-		Region:      config.Region,
-		Version:     "0.0.1",
-		KontrolURL:  kontrolURL,
-	}
-
-	return kite.New(options)
 }
