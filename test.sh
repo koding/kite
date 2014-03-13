8d8f468456fc88d73a236736ef787fd6cf2e1c40
Cenk Alti <cenkalti@gmail.com>
2014-03-12 17:14:59 -0700

    silence logs until -v flag is set in tests

--- test.sh
+++ test.sh
@@ -7,15 +7,24 @@ rm -rf $KITE_HOME
 export KITE_HOME=$KITE_HOME
 go run ./testutil/writekey/main.go
 
-go test -v ./dnode
-go test -v ./dnode/rpc
-go test -v ./cmd/cli
-go test -v ./systeminfo
-go test -v
-go test -v ./server
-go test -v ./registration/test
-go test -v ./regserv
-go test -v ./kontrol
-go test -v ./proxy
-go test -v ./simple
-go test -v ./pool
+while getopts “v” OPTION
+do
+     case $OPTION in
+         v)
+             VERBOSE="-v"
+             ;;
+     esac
+done
+
+go test $VERBOSE ./dnode
+go test $VERBOSE ./dnode/rpc
+go test $VERBOSE ./cmd/cli
+go test $VERBOSE ./systeminfo
+go test $VERBOSE
+go test $VERBOSE ./server
+go test $VERBOSE ./registration/test
+go test $VERBOSE ./regserv
+go test $VERBOSE ./kontrol
+go test $VERBOSE ./proxy
+go test $VERBOSE ./simple
+go test $VERBOSE ./pool
