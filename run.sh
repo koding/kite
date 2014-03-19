5ffcedd9e59e99b1c0ec25a6f302183d6de24b5b
Cenk Alti <cenkalti@gmail.com>
2014-03-19 15:39:06 -0700

    update run script

--- run.sh
+++ run.sh
@@ -9,23 +9,26 @@ set -x
 #
 
 # kill all running processes
-killall main || true
+killall main        || true
+killall math-simple || true
 
 # delete existing kite.key
 rm -rf $HOME/.kite
 
+# delete existing kontrol data
+rm -rf /tmp/kontrol-data
+
 # generate rsa keys
-openssl genrsa -out privateKey.pem 2048
-openssl rsa -in privateKey.pem -pubout > publicKey.pem
+openssl genrsa -out /tmp/privateKey.pem 2048
+openssl rsa -in /tmp/privateKey.pem -pubout > /tmp/publicKey.pem
 
 # initialize machine with new kite.key
-go run regserv/regserv/main.go -public-key publicKey.pem -private-key privateKey.pem -init -username devrim -kontrol-url "ws://localhost:4000"
+go run regserv/regserv/main.go -public-key /tmp/publicKey.pem -private-key /tmp/privateKey.pem -init -username devrim -kontrol-url "ws://localhost:4000"
 
 # run essential kites
-go run kontrol/kontrol/main.go -public-key publicKey.pem -private-key privateKey.pem &
-go run proxy/proxy/main.go -public-key publicKey.pem -private-key privateKey.pem &
-sleep 5
-go run regserv/regserv/main.go -public-key publicKey.pem -private-key privateKey.pem &
+go run kontrol/kontrol/main.go -public-key /tmp/publicKey.pem -private-key /tmp/privateKey.pem -data-dir /tmp/kontrol-data &
+go run proxy/proxy/main.go     -public-key /tmp/publicKey.pem -private-key /tmp/privateKey.pem &
+go run regserv/regserv/main.go -public-key /tmp/publicKey.pem -private-key /tmp/privateKey.pem &
 
 # run simple math kite
 go run examples/math-simple.go
