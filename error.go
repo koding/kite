95aff7f112b52d54f925f1793ec2a88d48b75f96
Cenk Alti <cenkalti@gmail.com>
2014-04-04 13:38:11 -0700

    add error.go

---
+++ error.go
@@ -0,0 +1,38 @@
+package kite
+
+import (
+	"fmt"
+
+	"github.com/koding/kite/dnode"
+)
+
+// MethodNotFoundError is returned when there is no registered handler for
+// received method.
+type MethodNotFoundError struct {
+	Method string
+	Args   *dnode.Partial
+}
+
+func (e MethodNotFoundError) Error() string {
+	return fmt.Sprintf("Method not found: %s", e.Method)
+}
+
+// CallbackNotFoundError is returned when there is no registered callback for
+// received message.
+type CallbackNotFoundError struct {
+	ID   uint64
+	Args *dnode.Partial
+}
+
+func (e CallbackNotFoundError) Error() string {
+	return fmt.Sprintf("Callback ID not found: %d", e.ID)
+}
+
+// ArgumentError is returned when received message contains invalid arguments.
+type ArgumentError struct {
+	s string
+}
+
+func (e ArgumentError) Error() string {
+	return e.s
+}
