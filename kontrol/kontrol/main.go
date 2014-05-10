7b942b5cc754c7bfa240e2dec0fa395ecaaf326e
Fatih Arslan <ftharsln@gmail.com>
2014-05-10 18:12:26 +0300

    kontrol: add initial flag for creating inital key on a new machine

--- kontrol/kontrol/main.go
+++ kontrol/kontrol/main.go
@@ -3,37 +3,45 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/url"
+	"os"
 	"strings"
 
 	"github.com/koding/kite/config"
 	"github.com/koding/kite/kontrol"
 )
 
+var (
+	// Kite server options
+	ip          = flag.String("ip", "0.0.0.0", "")
+	port        = flag.Int("port", 4000, "")
+	tlsCertFile = flag.String("tls-cert", "", "TLS certificate file")
+	tlsKeyFile  = flag.String("tls-key", "", "TLS key file")
+
+	// For self register and initial first key on a machine
+	initial    = flag.Bool("init", false, "create a new kite.key")
+	username   = flag.String("username", "", "")
+	kontrolURL = flag.String("kontrol-url", "", "")
+
+	// For signing/validating tokens
+	publicKeyFile  = flag.String("public-key", "", "Public RSA key")
+	privateKeyFile = flag.String("private-key", "", "Private RSA key")
+
+	// etcd instance options
+	name         = flag.String("name", "", "name of the instance")
+	dataDir      = flag.String("data-dir", "", "directory to store data")
+	etcdAddr     = flag.String("etcd-addr", "http://127.0.0.1:4001", "The public host:port used for etcd server.")
+	etcdBindAddr = flag.String("etcd-bind-addr", ":4001", "The listening host:port used for etcd server.")
+	peerAddr     = flag.String("peer-addr", "http://127.0.0.1:7001", "The public host:port used for peer communication.")
+	peerBindAddr = flag.String("peer-bind-addr", ":7001", "The listening host:port used for peer communication.")
+	peers        = flag.String("peers", "", "comma seperated peer addresses")
+	version      = flag.String("version", "0.0.1", "version of kontrol")
+)
+
 func main() {
-	var (
-		// Kite server options
-		ip          = flag.String("ip", "0.0.0.0", "")
-		port        = flag.Int("port", 4000, "")
-		tlsCertFile = flag.String("tls-cert", "", "TLS certificate file")
-		tlsKeyFile  = flag.String("tls-key", "", "TLS key file")
-
-		// For signing/validating tokens
-		publicKeyFile  = flag.String("public-key", "", "Public RSA key")
-		privateKeyFile = flag.String("private-key", "", "Private RSA key")
-
-		// etcd instance options
-		name         = flag.String("name", "", "name of the instance")
-		dataDir      = flag.String("data-dir", "", "directory to store data")
-		etcdAddr     = flag.String("etcd-addr", "http://127.0.0.1:4001", "The public host:port used for etcd server.")
-		etcdBindAddr = flag.String("etcd-bind-addr", ":4001", "The listening host:port used for etcd server.")
-		peerAddr     = flag.String("peer-addr", "http://127.0.0.1:7001", "The public host:port used for peer communication.")
-		peerBindAddr = flag.String("peer-bind-addr", ":7001", "The listening host:port used for peer communication.")
-		peers        = flag.String("peers", "", "comma seperated peer addresses")
-		version      = flag.String("version", "0.0.1", "version of kontrol")
-	)
-
 	flag.Parse()
 
 	if *publicKeyFile == "" {
@@ -52,6 +60,11 @@ func main() {
 	privateKey, err := ioutil.ReadFile(*privateKeyFile)
 	if err != nil {
 		log.Fatalf("cannot read private key file: %s", err.Error())
+	}
+
+	if *initial {
+		initialKey(publicKey, privateKey)
+		os.Exit(0)
 	}
 
 	conf := config.MustGet()
@@ -84,4 +97,27 @@ func main() {
 	}
 
 	k.Run()
+}
+
+func initialKey(publicKey, privateKey []byte) {
+	conf := config.New()
+
+	if *username == "" {
+		log.Fatalln("empty username")
+	}
+	conf.Username = *username
+
+	parsed, err := url.Parse(*kontrolURL)
+	if err != nil {
+		log.Fatalln("cannot parse kontrol URL")
+	}
+	conf.KontrolURL = parsed
+
+	k := kontrol.New(conf, *version, string(publicKey), string(privateKey))
+	err = k.RegisterSelf()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("kite.key is written to ~/.kite/kite.key. You can see it with:\n\tkite showkey")
 }
