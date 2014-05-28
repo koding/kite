2f22ddcc666f5284d3fda36aefe161cd12be3da6
Fatih Arslan <ftharsln@gmail.com>
2014-05-28 12:02:14 -0700

    run.sh: add a math kite that get's registered to kontrol

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ set -x
 
 # kill all running processes
 killall main        || true
-killall math-simple || true
+killall math-register || true
 
 # delete existing kite.key
 rm -rf $HOME/.kite
@@ -36,4 +36,4 @@ go run kontrol/kontrol/main.go -public-key /tmp/publicKey.pem -private-key /tmp/
 # go run proxy/proxy/main.go     -public-key /tmp/publicKey.pem -private-key /tmp/privateKey.pem &
 
 # run simple math kite
-go run examples/math/math.go
+go run examples/math/math-register.go
