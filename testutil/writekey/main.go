4c7cead4721503ff6d6a6ffcc03b62b1aa906906
Chris Thorn <morphcham@gmail.com>
2014-06-18 13:54:14 -0700

    add an option to print the kite key to stdout

--- testutil/writekey/main.go
+++ testutil/writekey/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/koding/kite/kitekey"
 	"github.com/koding/kite/testutil"
+	"os"
+)
+
+var (
+	flagEnableOutput = flag.Bool("stdout", false, "Output raw token to std out.")
 )
 
 func main() {
+	flag.Parse()
+
+	if *flagEnableOutput {
+		fmt.Printf("%v", testutil.NewKiteKey().Raw)
+		os.Exit(0)
+	}
+
 	kitekey.Write(testutil.NewKiteKey().Raw)
 }
