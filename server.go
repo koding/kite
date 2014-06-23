22bab09f1f0043d4b4d8d6d7e657334870b68d05
Fatih Arslan <ftharsln@gmail.com>
2014-06-23 03:01:10 -0700

    server: add graceful restart suppport

--- server.go
+++ server.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/rcrowley/goagain"
 )
 
 // Run is a blocking method. It runs the kite server and then accepts requests
@@ -58,25 +60,48 @@ func (k *Kite) Addr() string {
 // calls Serve to handle requests on incoming connectionk.
 func (k *Kite) listenAndServe() error {
 	var err error
-	k.listener, err = net.Listen("tcp4", k.Addr())
+
+	// inerhit a net.Listener from the parent process
+	k.listener, err = goagain.Listener()
 	if err != nil {
+		// create a new one if there doesn't exist
+		k.listener, err = net.Listen("tcp4", k.Addr())
+		if err != nil {
+			return err
+		}
+
+		k.Log.Info("New listening: %s", k.listener.Addr().String())
+
+		if k.TLSConfig != nil {
+			if k.TLSConfig.NextProtos == nil {
+				k.TLSConfig.NextProtos = []string{"http/1.1"}
+			}
+			k.listener = tls.NewListener(k.listener, k.TLSConfig)
+		}
+	} else {
+		k.Log.Info("Resuming listening on: %s", k.listener.Addr().String())
+
+		// Kill the parent, now that the child has started successfully.
+		if err := goagain.Kill(); nil != err {
+			k.Log.Fatal(err.Error())
+		}
+	}
+
+	// listener is ready, notify waiters.
+	close(k.readyC)
+
+	go func() {
+		defer close(k.closeC) // serving is finished, notify waiters.
+		k.Log.Info("Serving...")
+		http.Serve(k.listener, k)
+	}()
+
+	// Block the main goroutine awaiting signals. For a graceful restart we neeed SIGUSR2.
+	if _, err := goagain.Wait(k.listener); nil != err {
 		return err
 	}
 
-	k.Log.Info("Listening: %s", k.listener.Addr().String())
-
-	if k.TLSConfig != nil {
-		if k.TLSConfig.NextProtos == nil {
-			k.TLSConfig.NextProtos = []string{"http/1.1"}
-		}
-		k.listener = tls.NewListener(k.listener, k.TLSConfig)
-	}
-
-	close(k.readyC) // listener is ready, notify waiters.
-	k.Log.Info("Serving...")
-	defer close(k.closeC) // serving is finished, notify waiters.
-
-	return http.Serve(k.listener, k)
+	return nil
 }
 
 func (k *Kite) UseTLS(certPEM, keyPEM string) {
