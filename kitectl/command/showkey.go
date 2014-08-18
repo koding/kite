c3062b252ed8049c6c8cbb3085f482c48b3c90ea
H. İbrahim Güngör <igungor@gmail.com>
2014-08-18 07:55:19 +0300

    kitectl: Always show the keys in the same order.

--- kitectl/command/showkey.go
+++ kitectl/command/showkey.go
@@ -8,6 +8,16 @@ import (
 	"github.com/mitchellh/cli"
 )
 
+var tokenKeyOrder = []string{
+	"sub",        //  subject
+	"iss",        // issuer
+	"kontrolURL", // kontrol url
+	"aud",        // audience
+	"iat",        // issued at
+	"jti",        // JWT ID
+	"kontrolKey", // kontrol public key
+}
+
 type Showkey struct {
 	Ui cli.Ui
 }
@@ -40,8 +50,8 @@ func (c *Showkey) Run(_ []string) int {
 		return 1
 	}
 
-	for k, v := range token.Claims {
-		c.Ui.Output(fmt.Sprintf("%-15s%+v", k, v))
+	for _, v := range tokenKeyOrder {
+		c.Ui.Output(fmt.Sprintf("%-15s%+v", v, token.Claims[v]))
 	}
 
 	return 0
