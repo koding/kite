e999f7a28769de89130ee423ad353286db980f29
Fatih Arslan <ftharsln@gmail.com>
2014-10-22 19:27:30 +0300

    kontrol/postgres: split into two files so we are sure it works

--- kontrol/001-schema.sql
+++ kontrol/001-schema.sql
@@ -38,26 +38,3 @@ SELECT
         current_setting ('search_path') || ',kite',
         FALSE
     );
-
--- create the table
-CREATE TABLE "kite"."kite" (
-    username TEXT NOT NULL,
-    environment TEXT NOT NULL,
-    kitename TEXT NOT NULL,
-    version TEXT NOT NULL,
-    region TEXT NOT NULL,
-    hostname TEXT NOT NULL,
-    id uuid PRIMARY KEY,
-    url TEXT NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'), -- you may set a global timezone
-    updated_at timestamptz NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
-);
-
--- add proper permissions for table
-GRANT SELECT, INSERT, UPDATE, DELETE ON "kite"."kite" TO "kontrol";
-
-
--- create the index
-DROP INDEX IF EXISTS kite_updated_at_btree_idx;
-
-CREATE INDEX kite_updated_at_btree_idx ON "kite"."kite" USING BTREE (updated_at DESC);
