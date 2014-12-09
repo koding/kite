bdf9a68f68b852824ecb769ea1f9f5c39d2b47c9
Fatih Arslan <ftharsln@gmail.com>
2014-12-09 22:39:13 +0200

    oncevery: fix race condition

--- kontrol/onceevery/onceevery_test.go
+++ kontrol/onceevery/onceevery_test.go
@@ -1,6 +1,7 @@
 package onceevery
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -8,18 +9,25 @@ import (
 func TestOnceEvery(t *testing.T) {
 	once := New(time.Second)
 	count := 0
+	var countMu sync.Mutex
 
 	go func() {
 		for i := 0; i < 100; i++ {
 			once.Do(func() {
+				countMu.Lock()
 				count++
+				countMu.Unlock()
 			})
 		}
 	}()
 
 	time.Sleep(time.Second * 2)
 
+	countMu.Lock()
+	defer countMu.Unlock()
+
 	if count != 2 {
 		t.Errorf("function should be calle two times, got '%d'", count)
 	}
+
 }
